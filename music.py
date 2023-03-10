11cb1be0ac3058cfa39964bb2fb4cda3711997ac
KendallDoesCoding <kendalldoescoding@gmail.com>
2023-03-10 21:33:28 +0530

    fix: make music volume softer
    fixes #26
    
    Co-authored-by: NotHereToRule <sahiltomar100303@gmail.com>
    Co-authored-by: Sahil Singh Tomar <50849361+SahilSinghTomar@users.noreply.github.com>

--- music.py
+++ music.py
@@ -6,4 +6,5 @@ def music():
     pygame.init()
     pygame.mixer.init()
     sound = pygame.mixer.Sound("Music/sotb.mp3")
+    sound.set_volume(0.2)              # Now plays at 20% of full volume.
     sound.play()
