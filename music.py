c66418ef634f8b726fd2f9d02592eed9c51753c6
KendallDoesCoding <kendalldoescoding@gmail.com>
2023-03-10 21:32:51 +0530

    chore: shift music to pygame

--- music.py
+++ music.py
@@ -1,4 +1,9 @@
 def music():
-    from playsound import playsound
-    playsound('Music/sotb.mp3', False)
-    print("Music has started.")
+    import os
+    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+    import pygame
+
+    pygame.init()
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound("Music/sotb.mp3")
+    sound.play()
