bd6709ef6961a601f6341193c2333f86228bf592
Omppu42 <51288885+Omppu42@users.noreply.github.com>
2023-03-12 17:02:39 +0200

    Apply restyled patch

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 # import modules
-import sys
 import subprocess
+import sys
+
 import pkg_resources
 
+from chapters import *
+from music_player import *
+
 # install missing modules
-required = {'playsound==1.2.2', 'colorama==0.4.6'}
+required = {"playsound==1.2.2", "colorama==0.4.6"}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
 if missing:
     python = sys.executable
-    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+    subprocess.check_call([python, "-m", "pip", "install", *missing],
+                          stdout=subprocess.DEVNULL)
 
 # import dependencies
-from chapters import *
-from music_player import *
 
 # heading text!
-heading = 'Choose Your Own Adventure Game!'
+heading = "Choose Your Own Adventure Game!"
 copyright = "\U000000A9 2023, KendallDoesCoding, All Rights Reserved"
 new_str = Fore.BLUE + heading.center(150)
 new_str2 = Fore.BLUE + copyright.center(150)
 print(new_str)
 print(new_str2)
 
+
 def main():
     # welcome to the game
     name = input(Fore.YELLOW + "Type your name: " + Fore.LIGHTBLUE_EX)
-    print(Fore.LIGHTGREEN_EX + "Welcome", name, "to this adventure!" )
+    print(Fore.LIGHTGREEN_EX + "Welcome", name, "to this adventure!")
 
     # do you want to play?
-    answer = input(Fore.YELLOW + "Do you want to play? (y/n) " + Fore.LIGHTBLUE_EX)
+    answer = input(Fore.YELLOW + "Do you want to play? (y/n) " +
+                   Fore.LIGHTBLUE_EX)
     if answer == "y" or answer == "yes":
         # starting the game
         print(Fore.LIGHTGREEN_EX + "Let's play! \U0001F3AE")
@@ -38,7 +43,8 @@ def main():
         print("See you later! \U0001F600")
         exit()
     # do you want music?
-    answer = input(Fore.YELLOW + "Do you want music? \U0001F3B5 (y/n) " + Fore.LIGHTBLUE_EX)
+    answer = input(Fore.YELLOW + "Do you want music? \U0001F3B5 (y/n) " +
+                   Fore.LIGHTBLUE_EX)
     if answer == "y" or answer == "yes":
         music()
         random.choice(my_list)()
@@ -47,8 +53,5 @@ def main():
         random.choice(my_list)()
 
 
-
 if __name__ == "__main__":
     main()
-
-
